d9321cbf5440a36709482da66a7e9d888ed495da
Nathan Miller <nathan_miller23@berkeley.edu>
2020-08-11 15:14:46 -0500

    removed uneccessary imports

--- testing/__init__.py
+++
@@ -1,5 +0,0 @@
-import os
-from testing.common_test_generator import generate_serialized_trajectory
-
-_current_dir = os.path.dirname(os.path.abspath(__file__))
-TESTING_DATA_DIR = os.path.join(_current_dir, os.pardir, 'overcooked_ai_py', 'data', 'testing')
