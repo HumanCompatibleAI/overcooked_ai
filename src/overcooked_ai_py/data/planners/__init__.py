82dbd0789b7d00aec1896c1bd7c0d9d8ea7cbc16
Nathan Miller <nathan_miller23@berkeley.edu>
2020-08-11 13:45:13 -0500

    fixed gitignore

---
+++ src/overcooked_ai_py/data/planners/__init__.py
@@ -0,0 +1,8 @@
+import os, pickle
+from overcooked_ai_py.utils import load_dict_from_file
+from overcooked_ai_py.static import PLANNERS_DIR
+
+def load_saved_action_manager(filename):
+    with open(os.path.join(PLANNERS_DIR, filename), 'rb') as f:
+        mlp_action_manager = pickle.load(f)
+        return mlp_action_manager
