82ac6c40add798410970a555cac6b4220c2c37ad
bmielnicki <bartlomiej.mielnicki@gmail.com>
2020-08-13 10:04:13 +0200

    cleaning unused files after merge

--- overcooked_ai_py/run_tests.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import argparse
-from overcooked_ai_py.mdp.overcooked_test import *
-from overcooked_ai_py.planning.planners_test import TestMotionPlanner, TestMediumLevelPlanner, TestJointMotionPlanner, TestHighLevelPlanner
-from overcooked_ai_py.agents.agent_test import TestAgentEvaluator, TestBasicAgents
-from overcooked_ai_py.visualization.visualization_test import TestVisualizations
-
-if __name__ == '__main__':
-    unittest.main()
--- overcooked_ai_py/run_tests_full_planning_suite.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import argparse
-from overcooked_ai_py.mdp.overcooked_test import *
-from overcooked_ai_py.planning.planners_test import *
-from overcooked_ai_py.agents.agent_test import *
-from overcooked_ai_py.visualization.visualization_test import *
-
-if __name__ == '__main__':
-    unittest.main()
