8f258d8981dbd29bbd1681b244a87a141b6ad93b
bmielnicki <bartlomiej.mielnicki@gmail.com>
2020-08-18 13:09:08 +0200

    renamed one letter variables

--- src/overcooked_ai_py/visualization/extract_events.py
+++ src/overcooked_ai_py/visualization/extract_events.py
@@ -7,32 +7,32 @@ def extract_events(trajectories, traj_idx=0, add_cumulative_events=True):
     extracts events from trajectories for plots
     """
 
-    def find_last_event(events, event_type, p, last_timestep=None):
+    def find_last_event(events, event_type, player_num, last_timestep=None):
         for event in reversed(events):
-            if event.get("player") == p and event.get("action") == event_type \
+            if event.get("player") == player_num and event.get("action") == event_type \
                     and (last_timestep is None or event["timestep"] < last_timestep):
                 return event
             
-    def holding_event(events, p, t, obj):
-        last_pickup_t = (find_last_event(events, "pickup", p, t) or {}).get("timestep")
+    def holding_event(events, player_num, timestep, obj):
+        last_pickup_t = (find_last_event(events, "pickup", player_num, timestep) or {}).get("timestep")
 
         start_t = last_pickup_t or 0
         event = {
-            "player": p,
+            "player": player_num,
             "start_timestep":start_t,
-            "end_timestep":t,
+            "end_timestep":timestep,
             "action": "holding",
             "object": obj
         }
         return event
 
     def get_holding_events(events, game_states):
-        holding_events = [holding_event(events, e.get("player"), e.get("timestep"), e.get("object")) 
-            for e in events if e.get("action") in ["drop", "potting", "delivery"]]
+        holding_events = [holding_event(events, event.get("player"), event.get("timestep"), event.get("object")) 
+            for event in events if event.get("action") in ["drop", "potting", "delivery"]]
         # check if object is held at end of the episode
-        for p, player in enumerate(game_states[-1].players):
+        for player_num, player in enumerate(game_states[-1].players):
             if player.held_object:
-                holding_events.append(holding_event(events, p, len(game_states)-1, player.held_object.to_dict()))
+                holding_events.append(holding_event(events, player_num, len(game_states)-1, player.held_object.to_dict()))
         return holding_events
 
 
@@ -40,38 +40,38 @@ def extract_events(trajectories, traj_idx=0, add_cumulative_events=True):
         """
         Receives events for scatter plot and returns events for cumulative plot
         """
-        def add_cumulative_event(events, s, timestep, action, player=None, obj=None):
+        def add_cumulative_event(events, events_sum, timestep, action, player_num=None, obj=None):
             event = {"action": action,
-                    "sum": s,
+                    "sum": events_sum,
                     "timestep": timestep,}
-            if player is not None: event["player"] = player
+            if player_num is not None: event["player"] = player_num
             if obj is not None: event["object"] = obj
             events.append(event)
 
         cumulative_events = []
 
-        s = 0
+        all_events_sum = 0
         player_sums = defaultdict(int)
-        a = "pickups_and_drops"
+        action_name = "pickups_and_drops"
         
         # add zero timestep events
         first_timestep = 0
-        add_cumulative_event(cumulative_events, s, first_timestep, a)
-        for p in set(map(lambda x: x["player"], events)):
-            add_cumulative_event(cumulative_events, s, first_timestep, a, p)
+        add_cumulative_event(cumulative_events, all_events_sum, first_timestep, action_name)
+        for player_num in set(map(lambda x: x["player"], events)):
+            add_cumulative_event(cumulative_events, all_events_sum, first_timestep, action_name, player_num)
 
-        for e in filter(lambda x:x["action"] in ["pickup", "drop"], events):
-            p = e["player"]
-            t = e["timestep"]
-            s+=1
-            player_sums[p]+=1
-            add_cumulative_event(cumulative_events, s, t, a)
-            add_cumulative_event(cumulative_events, player_sums[p], t, a, p)
+        for event in filter(lambda x:x["action"] in ["pickup", "drop", "delivery", "potting"], events):
+            player_num = event["player"]
+            timestep = event["timestep"]
+            all_events_sum +=1
+            player_sums[player_num] +=1
+            add_cumulative_event(cumulative_events, all_events_sum, timestep, action_name)
+            add_cumulative_event(cumulative_events, player_sums[player_num], timestep, action_name, player_num)
 
         # add cumulative events at last timestep for nice graph
-        add_cumulative_event(cumulative_events, s, last_timestep, a)
-        for p in set(map(lambda x: x["player"], events)):
-            add_cumulative_event(cumulative_events, player_sums[p], last_timestep, a, p)
+        add_cumulative_event(cumulative_events, all_events_sum, last_timestep, action_name)
+        for player_num in set(map(lambda x: x["player"], events)):
+            add_cumulative_event(cumulative_events, player_sums[player_num], last_timestep, action_name, player_num)
         return cumulative_events
         
     ep_states = trajectories["ep_states"][traj_idx]
@@ -86,5 +86,3 @@ def extract_events(trajectories, traj_idx=0, add_cumulative_events=True):
     events = [{k:numpy_to_native(v) for k,v in event.items()} for event in events]
     
     return events
-
-
